0a93cfe4b26f1ed0e9e21e46ad5ae46678cbac1c
John Sandfort <garnavis@gmail.com>
2014-01-23 23:22:08 -0500

    Barely beginning the reader class.

---
+++ src/java/us/jsand/miaul/AbcReader.java
@@ -0,0 +1,19 @@
+package us.jsand.maul;
+
+import com.audiveris.proxymusic.*;
+import com.audiveris.proxymusic.util.*;
+import com.audiveris.proxymusic.ScorePartwise.Part;
+import com.audiveris.proxymusic.ScorePartwise.Part.Measure;
+
+public class AbcReader extends MiaulReader {
+    /**
+     * Read in some abc notation and return a MusicXML document.
+     * @param in An <code>InputStream</code> of any kind.
+     * @return The abc document represented as a MusicXML document.
+     */
+    @Override
+    public ScorePartwise read(InputStream in) {
+        // ...okay, read
+    }
+}
+
---
+++ src/java/us/jsand/miaul/MiaulReader.java
@@ -0,0 +1,18 @@
+package us.jsand.maiul;
+
+import com.audiveris.proxymusic.*;
+import com.audiveris.proxymusic.util.*;
+import com.audiveris.proxymusic.ScorePartwise.Part;
+import com.audiveris.proxymusic.ScorePartwise.Part.Measure;
+
+abstract class MiaulReader {
+    /**
+     * Read in some plain-text music representation and return a
+     * <code>ScorePartwise</code> object, which can be marshalled to a MusicXML
+     * document.
+     * @param in An <code>InputStream</code>, to be read char-by-char.
+     * @return A MusicXML translation of the input.
+     */
+    abstract public ScorePartwise read(InputStream in) {};
+}
+
