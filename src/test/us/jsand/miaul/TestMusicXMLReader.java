d0705f81be1f7c43fd40243a05b35899acbca6d3
John Sandfort <garnavis@gmail.com>
2014-02-10 22:56:52 -0500

    I don't remember what's in this commit.

---
+++ src/test/us/jsand/miaul/TestMusicXMLReader.java
@@ -0,0 +1,14 @@
+package us.jsand.miaul;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+
+public class TestMusicXMLReader {
+    @Test
+    testRead() {
+        MusicXMLReader reader = new MusicXMLReader();
+    }
+}
