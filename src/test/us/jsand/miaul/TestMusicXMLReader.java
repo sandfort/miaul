92d458228d0127de5df24b8a189a24928e170651
John Sandfort <garnavis@gmail.com>
2014-02-18 15:21:28 -0500

    TestMusicXMLReader is passing\!

--- src/test/us/jsand/miaul/TestMusicXMLReader.java
+++ src/test/us/jsand/miaul/TestMusicXMLReader.java
@@ -9,8 +9,8 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
 
-import com.audiveris.proxmusic;
-import com.audiveris.proxymusic.ScorePartwise;
+import com.audiveris.proxymusic.*;
+import com.audiveris.proxymusic.util.*;
 import com.audiveris.proxymusic.ScorePartwise.Part;
 import com.audiveris.proxymusic.ScorePartwise.Part.Measure;
 
@@ -20,6 +20,9 @@ import org.junit.After;
 
 public class TestMusicXMLReader {
 
+    public static final String FILE_SEPARATOR = 
+                System.getProperty("file.separator");
+
     MusicXMLReader reader;
     String res;
     String in;
@@ -27,12 +30,15 @@ public class TestMusicXMLReader {
     @Before
     public void setUp() {
         reader = new MusicXMLReader();
-        res = this.getClass().getResource("/res").toString();
-        in = res + File.separator + "helloworld.xml";
+        in = "src/test/res/helloworld.xml";
 
         ObjectFactory factory = new ObjectFactory();
 
+        // A score where measures are arranged in parts.
+        // Use ScoreTimewise for a score where parts are arranged in measures.
         ScorePartwise score = factory.createScorePartwise();
+
+        // Establish a list of parts.
         PartList parts = factory.createPartList();
         score.setPartList(parts);
         ScorePart scorePart = factory.createScorePart();
@@ -41,33 +47,53 @@ public class TestMusicXMLReader {
         name.setValue("Part 1");
         scorePart.setPartName(name);
         parts.getPartGroupOrScorePart().add(scorePart);
+
+        // Now create the part that lives in the score.
         Part part = factory.createScorePartwisePart();
+        // connect the part to the score by reference, not by ID!
         part.setId(scorePart);
         score.getPart().add(part);
+
+        // Create the measure that lives in the part.
         Measure measure = factory.createScorePartwisePartMeasure();
         part.getMeasure().add(measure);
         measure.setNumber("1");
+
+        // Set up the attributes of the measure.
         Attributes attr = factory.createAttributes();
         measure.getNoteOrBackupOrForward().add(attr);
         attr.setDivisions(new BigDecimal(1));
+
         Key keyOfC = factory.createKey();
         keyOfC.setFifths(new BigInteger("0"));
         attr.getKey().add(keyOfC);
+
+        // This is really important
         Time commonTime = factory.createTime();
+        // Particularly these two lines
         commonTime.getTimeSignature().add(factory.createTimeBeats("4"));
         commonTime.getTimeSignature().add(factory.createTimeBeatType("4"));
         attr.getTime().add(commonTime);
+
         Clef gClef = new Clef();
         attr.getClef().add(gClef);
         gClef.setSign(ClefSign.G);
         gClef.setLine(new BigInteger("2"));
+
+        // Now make an actual note
         Note note = factory.createNote();
         measure.getNoteOrBackupOrForward().add(note);
+
+        // Set the pitch of the note
         Pitch pitch = factory.createPitch();
         note.setPitch(pitch);
         pitch.setStep(Step.C);
         pitch.setOctave(4);
+
+        // Set the duration of the note
         note.setDuration(new BigDecimal(4));
+
+        // Set the type of the note
         NoteType type = factory.createNoteType();
         type.setValue("whole");
         note.setType(type);
@@ -76,6 +102,12 @@ public class TestMusicXMLReader {
     @Test
     public void testRead() {
         MusicXMLReader reader = new MusicXMLReader();
-        ScorePartwise result = reader.read(new FileInputStream(in));
+        try {
+            ScorePartwise result = reader.read(new FileInputStream(in));
+        } catch (FileNotFoundException fnfe) {
+            fail(fnfe.toString());
+        } catch (MiaulReaderException mre) {
+            fail(mre.toString());
+        }
     }
 }
